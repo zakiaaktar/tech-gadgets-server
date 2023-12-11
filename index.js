ec9b0d1023bab241d68f7e9701893b2013867545
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-12-11 13:10:12 +0100

    delete cart part done

--- index.js
+++ index.js
@@ -72,11 +72,18 @@ async function run() {
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
-    
+
     app.post('/carts', async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result)
+    });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     });
 
 
