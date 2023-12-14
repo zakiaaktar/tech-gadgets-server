9dd5191c09d48290061071e6db7005edf07653a4
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-12-14 20:05:46 +0100

    jwt part done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 
@@ -38,8 +39,36 @@ async function run() {
     const userCollection = client.db("techDb").collection("users");
 
 
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' });
+      res.send({ token });
+    });
+
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      //console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+
+
+
      // users related api
-     app.get('/users', async (req, res) => {
+     app.get('/users', verifyToken, async (req, res) => {
+        
       const result = await userCollection.find().toArray();
       res.send(result);
     });
@@ -56,6 +85,20 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
 
 
     app.delete('/users/:id', async (req, res) => {
