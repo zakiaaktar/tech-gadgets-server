cae27e6d48da64b91b65ba9e406cbdffd9f33314
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-12-14 20:47:14 +0100

    verifyAdmin part done

--- index.js
+++ index.js
@@ -64,14 +64,44 @@ async function run() {
     }
 
 
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
 
 
      // users related api
-     app.get('/users', verifyToken, async (req, res) => {
-        
+     app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+       //console.log(req.headers);
       const result = await userCollection.find().toArray();
       res.send(result);
     });
+
+
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
 
 
      app.post('/users', async (req, res) => {
@@ -87,7 +117,7 @@ async function run() {
     });
 
 
-    app.patch('/users/admin/:id', async (req, res) => {
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -101,7 +131,7 @@ async function run() {
 
 
 
-    app.delete('/users/:id', async (req, res) => {
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query);
